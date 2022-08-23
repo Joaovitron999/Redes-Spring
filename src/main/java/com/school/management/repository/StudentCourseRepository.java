43f6639fd7239dc1df15ce659de60d4ca70dbded
Joaovitron999 <jv.fonseca999@gmail.com>
2022-08-23 15:25:59 -0300

    StudentCourse and StudentCourseDto

---
+++ src/main/java/com/school/management/repository/StudentCourseRepository.java
@@ -0,0 +1,12 @@
+package com.school.management.repository;
+
+import com.school.management.model.StudentCourse;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface StudentCourseRepository extends JpaRepository<StudentCourse, Long> {
+}
